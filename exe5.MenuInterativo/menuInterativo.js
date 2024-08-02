af2fd832a5d17cf7bf26295a8d1af7c731d7f13e
Ane Monteiro <anerebecaluz@gmail.com>
2024-08-02 08:56:02 -0300

    novo exercício

---
+++ exe5.MenuInterativo/menuInterativo.js
@@ -0,0 +1,6 @@
+let opcao;
+do {
+    opcao = prompt("Escolha seu esporte preferido, nossas opções são:\n1- Vôlei\n2- Futebol\n3- Basquete\n4- Handebol\n5- Encerrar (para finalizar a pesquisa)");
+} while (opcao !== "5")
+
+alert("Você encerrou a pesquisa. Obrigada por participar!");
