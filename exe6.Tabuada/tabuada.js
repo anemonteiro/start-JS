21c60d564454b7174ca16d1d728d79f12be5a675
Ane Monteiro <anerebecaluz@gmail.com>
2024-08-09 10:37:19 -0300

    Tabuada treinando o uso do FOR

---
+++ exe6.Tabuada/tabuada.js
@@ -0,0 +1,12 @@
+let number = parseFloat(prompt("Digite um número para descobrir sua tabuada:"));
+let multiplicacao = [];
+
+if (number === 0) {
+    alert("Todo número multiplicado por 0 = 0.")
+} else { 
+    for (i = 1; i <= 20; i++) {
+        multiplicacao.push(`${number} X ${i} = ${number * i}`);
+    }
+}
+
+alert(`Resultado da Multiplicação:\n${multiplicacao.join(`\n`)}`);
