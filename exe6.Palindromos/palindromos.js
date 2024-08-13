6a8e04d14d9752116a7c369c1dd11ed23b64d585
Ane Monteiro <anerebecaluz@gmail.com>
2024-08-13 08:31:59 -0300

    ajustes

--- exe6.Palindromos/palindromos.js
+++ exe6.Palindromos/palindromos.js
@@ -1,11 +1,9 @@
-/*
-Escreva um programa em javascript que seja capaz de identificar se uma palavra é um palíndromo. Um palíndromo é uma palavra que lida de trás para frente possui as mesmas letras na mesma ordem. O programa deve iniciar pedindo que seja informada uma palavra e então deve exibir uma mensagem dizendo se aquela palavra é ou não um palíndromo. Caso não seja um palíndromo, o programa deve mostrar a palavra lida da esquerda para direita e da direita para esquerda.
-*/
+const palavraInformada = prompt("Informe a palavra que deseja analisar:").toLowerCase();
 
-const palavraInformada = prompt("Informe a palavra que deseja analisar:");
 let palavraPalindromo = [];
 
-for (i = palavraInformada.length; i >= 0; i--) {
+
+for (i = palavraInformada.length-1; i >= 0; i--) {
     palavraPalindromo.push(palavraInformada[i]);
 }
 
