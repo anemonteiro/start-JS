5c8e1e4b8c028276a5fcc6f83b3f1d5f007fe488
Ane Monteiro <anerebecaluz@gmail.com>
2024-08-29 10:13:32 -0300

    Exercício imobiliária

--- exe8.CadastroImóveis/imoveis.js
+++ exe8.CadastroImóveis/imoveis.js
@@ -1,5 +1,5 @@
+imoveisCadastrados = [];
 let menu;
-let imoveisList = [];
 
 do {
     menu = prompt(`Quantidade de imóveis cadastrados: ${imoveisCadastrados.length}` + "\n\nVocê deseja:\n1- Salvar um novo imóvel\n2- Ver todos os imóveis\n3- Sair");
@@ -9,18 +9,26 @@ do {
             let novoImovel = {}
             novoImovel.proprietario = prompt("Nome do proprietário:"),
             novoImovel.quartos = parseInt(prompt("Quantos quartos?")),
-            novoImovel.banheiros = parseInt(prompt("Quantos banheiros?"))
+            novoImovel.banheiros = parseInt(prompt("Quantos banheiros?")),
             novoImovel.garagem = prompt("Possui garagem? (s/n)").toLowerCase();
-            
 
-
-        
             imoveisCadastrados.push(novoImovel);
-            alert("Imóvel cadastrado com sucesso!")
+            alert("Imóvel cadastrado com sucesso!");
             break;
 
         case "2":
+            let lista = "";
+            for(let i = 0; i < imoveisCadastrados.length; i++) {
+                let possuiGaragem = imoveisCadastrados[i].garagem === "s" ? "Sim" : "Não";
 
+                lista += `Imóvel ${i+1}:\n` +
+                `Proprietário: ${imoveisCadastrados[i].proprietario}, ` + 
+                `Quartos: ${imoveisCadastrados[i].quartos}, ` +
+                `Banheiros: ${imoveisCadastrados[i].banheiros}, ` +
+                `Garagem: ${possuiGaragem}\n\n`
+            }
+
+            alert(lista);
             break;
         
         case "3":
